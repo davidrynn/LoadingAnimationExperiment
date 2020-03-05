b050e2af6708d46064d2aedec793366ba51e54f9
David Rynn <davidrynn@gmail.com>
2020-03-05 01:17:38 -0500

    Initial Commit

---
+++ LoadingAnimationExperient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingAnimationExperient
+//
+//  Created by David Rynn on 3/5/20.
+//  Copyright © 2020 David Rynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
